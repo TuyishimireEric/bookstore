88496b5d32857f62cafb4d3fe96ca38c69d7b72c
Tuyishimire Eric~ <tuyishimireericc@gmail.com>
2022-08-23 12:18:40 +0200

    added redux

---
+++ src/redux/categories/categories.js
@@ -0,0 +1,18 @@
+const CHECK_STATUS = 'CHECK_STATUS';
+
+const initialState = [];
+
+const checkReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CHECK_STATUS:
+      return 'Under construction';
+    default:
+      return state;
+  }
+};
+
+export const checkStatus = () => ({
+  type: CHECK_STATUS,
+});
+
+export default checkReducer;
